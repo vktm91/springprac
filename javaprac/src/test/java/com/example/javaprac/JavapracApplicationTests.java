4297c6a82c02b4d23ec1fb24f95007f582c61a3f
vktm91@nate.com <vktm91@nate.com>
2021-10-13 17:16:13 +0900

    캐스팅 연습

---
+++ javaprac/src/test/java/com/example/javaprac/JavapracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavapracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
