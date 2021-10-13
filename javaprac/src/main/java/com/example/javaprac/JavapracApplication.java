ad3e671447f9a611ee3322690099e967d6dd271b
vktm91@nate.com <vktm91@nate.com>
2021-10-13 17:16:47 +0900

    캐스팅 연습

---
+++ javaprac/src/main/java/com/example/javaprac/JavapracApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavapracApplication.class, args);
+    }
+
+}
